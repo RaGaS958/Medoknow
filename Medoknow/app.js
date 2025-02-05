d56157e99df670ae2c25391a0ac19dafa460c5cf
RaGaS958 <pushkarkhattri111@gmail.com>
2025-02-05 09:20:03 +0530

    base

---
+++ Medoknow/app.js
@@ -0,0 +1,17 @@
+if (window.VANTA) {
+
+    VANTA.WAVES({
+        el: "#nav",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 300.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x365b98,
+        shininess: 81.00,
+        waveSpeed: 0.65,
+        zoom: 1.44
+      })
+}
